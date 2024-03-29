e2bab865d6f0782a92c91011b536850d04db604f
mike76-dev <michael.bulanov@yahoo.com>
2024-03-29 17:18:50 +0100

    Display a message when the host's location is unknown

--- web/src/components/HostMap/HostMap.tsx
+++ web/src/components/HostMap/HostMap.tsx
@@ -19,26 +19,29 @@ export const HostMap = (props: HostMapProps) => {
     }
     return (
         <div className={'host-map-container' + (props.darkMode ? ' host-map-dark' : '')}>
-            <MapContainer
-                center={geolocation(props.host.loc)}
-                zoom={7}
-                scrollWheelZoom={false}>
-                <TileLayer
-                    attribution={props.darkMode ?
-                        '&copy; <a href="https://carto.com/attributions">Carto</a> contributors' :
-                        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    }
-                    url={props.darkMode ?
-                        'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png' :
-                        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-                    }
-                />
-                <Marker position={geolocation(props.host.loc)}>
-                    <Popup className="host-map-popup">
-                        {props.host.city + ', ' + props.host.region + ', ' + props.host.country}
-                    </Popup>
-                </Marker>
-            </MapContainer>
+            {props.host.loc !== '' ?
+                <MapContainer
+                    center={geolocation(props.host.loc)}
+                    zoom={7}
+                    scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution={props.darkMode ?
+                            '&copy; <a href="https://carto.com/attributions">Carto</a> contributors' :
+                            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        }
+                        url={props.darkMode ?
+                            'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png' :
+                            'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                        }
+                    />
+                    <Marker position={geolocation(props.host.loc)}>
+                        <Popup className="host-map-popup">
+                            {props.host.city + ', ' + props.host.region + ', ' + props.host.country}
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            : <div className="host-map-unknown">Location unknown</div>
+            }
         </div>
     )
 }
