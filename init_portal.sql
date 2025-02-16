eb3c03146b9f43341b7ff3725075b93a3470d7a6
mike76-dev <michael.bulanov@yahoo.com>
2025-02-16 14:48:49 +0100

    Format

--- init_portal.sql
+++ init_portal.sql
@@ -16,17 +16,17 @@ CREATE TABLE hosts (
 	net_address        VARCHAR(255) NOT NULL,
 	ip_nets            TEXT NOT NULL,
 	last_ip_change     BIGINT NOT NULL,
-    price_score        DOUBLE NOT NULL,
-    storage_score      DOUBLE NOT NULL,
-    collateral_score   DOUBLE NOT NULL,
-    interactions_score DOUBLE NOT NULL,
-    uptime_score       DOUBLE NOT NULL,
-    age_score          DOUBLE NOT NULL,
-    version_score      DOUBLE NOT NULL,
-    latency_score      DOUBLE NOT NULL,
-    benchmarks_score   DOUBLE NOT NULL,
-    contracts_score    DOUBLE NOT NULL,
-    total_score        DOUBLE NOT NULL,
+	price_score        DOUBLE NOT NULL,
+	storage_score      DOUBLE NOT NULL,
+	collateral_score   DOUBLE NOT NULL,
+	interactions_score DOUBLE NOT NULL,
+	uptime_score       DOUBLE NOT NULL,
+	age_score          DOUBLE NOT NULL,
+	version_score      DOUBLE NOT NULL,
+	latency_score      DOUBLE NOT NULL,
+	benchmarks_score   DOUBLE NOT NULL,
+	contracts_score    DOUBLE NOT NULL,
+	total_score        DOUBLE NOT NULL,
 	settings           BLOB,
 	price_table        BLOB,
 	PRIMARY KEY (id, network)
@@ -39,24 +39,24 @@ CREATE TABLE interactions (
 	uptime             BIGINT NOT NULL,
 	downtime           BIGINT NOT NULL,
 	last_seen          BIGINT NOT NULL,
-    active_hosts       INT NOT NULL,
-    price_score        DOUBLE NOT NULL,
-    storage_score      DOUBLE NOT NULL,
-    collateral_score   DOUBLE NOT NULL,
-    interactions_score DOUBLE NOT NULL,
-    uptime_score       DOUBLE NOT NULL,
-    age_score          DOUBLE NOT NULL,
-    version_score      DOUBLE NOT NULL,
-    latency_score      DOUBLE NOT NULL,
-    benchmarks_score   DOUBLE NOT NULL,
-    contracts_score    DOUBLE NOT NULL,
-    total_score        DOUBLE NOT NULL,
+	active_hosts       INT NOT NULL,
+	price_score        DOUBLE NOT NULL,
+	storage_score      DOUBLE NOT NULL,
+	collateral_score   DOUBLE NOT NULL,
+	interactions_score DOUBLE NOT NULL,
+	uptime_score       DOUBLE NOT NULL,
+	age_score          DOUBLE NOT NULL,
+	version_score      DOUBLE NOT NULL,
+	latency_score      DOUBLE NOT NULL,
+	benchmarks_score   DOUBLE NOT NULL,
+	contracts_score    DOUBLE NOT NULL,
+	total_score        DOUBLE NOT NULL,
 	successes          DOUBLE NOT NULL,
 	failures           DOUBLE NOT NULL,
 	last_update        BIGINT UNSIGNED NOT NULL,
 	PRIMARY KEY (network, node, public_key),
-    FOREIGN KEY (public_key) REFERENCES hosts(public_key),
-    INDEX idx_interactions (network, public_key)
+	FOREIGN KEY (public_key) REFERENCES hosts(public_key),
+	INDEX idx_interactions (network, public_key)
 );
 
 CREATE TABLE scans (
@@ -69,8 +69,8 @@ CREATE TABLE scans (
 	latency    DOUBLE NOT NULL,
 	error      TEXT NOT NULL,
 	PRIMARY KEY (id),
-    FOREIGN KEY (public_key) REFERENCES hosts(public_key),
-    INDEX idx_scans (network, node, public_key, ran_at)
+	FOREIGN KEY (public_key) REFERENCES hosts(public_key),
+	INDEX idx_scans (network, node, public_key, ran_at)
 );
 
 CREATE TABLE benchmarks (
@@ -85,26 +85,26 @@ CREATE TABLE benchmarks (
 	ttfb           DOUBLE NOT NULL,
 	error          TEXT NOT NULL,
 	PRIMARY KEY (id),
-    FOREIGN KEY (public_key) REFERENCES hosts(public_key)
+	FOREIGN KEY (public_key) REFERENCES hosts(public_key)
 );
 
 CREATE TABLE price_changes (
-    id                BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    network           VARCHAR(8) NOT NULL,
-    public_key        BINARY(32) NOT NULL,
-    changed_at        BIGINT NOT NULL,
-    remaining_storage BIGINT UNSIGNED NOT NULL,
-    total_storage     BIGINT UNSIGNED NOT NULL,
-    collateral        TINYBLOB NOT NULL,
-    storage_price     TINYBLOB NOT NULL,
-    upload_price      TINYBLOB NOT NULL,
-    download_price    TINYBLOB NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (public_key) REFERENCES hosts(public_key)
+	id                BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	network           VARCHAR(8) NOT NULL,
+	public_key        BINARY(32) NOT NULL,
+	changed_at        BIGINT NOT NULL,
+	remaining_storage BIGINT UNSIGNED NOT NULL,
+	total_storage     BIGINT UNSIGNED NOT NULL,
+	collateral        TINYBLOB NOT NULL,
+	storage_price     TINYBLOB NOT NULL,
+	upload_price      TINYBLOB NOT NULL,
+	download_price    TINYBLOB NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (public_key) REFERENCES hosts(public_key)
 );
 
 CREATE TABLE locations (
-    network    VARCHAR(8) NOT NULL,
+	network    VARCHAR(8) NOT NULL,
 	public_key BINARY(32) NOT NULL,
 	ip         TEXT NOT NULL,
 	host_name  TEXT NOT NULL,
